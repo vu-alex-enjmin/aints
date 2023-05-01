85366696f7898822ce4b6e91e7be5c433076d0a2
vu-danny <dan.vu.alx@gmail.com>
2023-05-01 19:41:38 +0200

    Comment Square

--- src/core/Square.h
+++ src/core/Square.h
@@ -16,11 +16,26 @@ struct Square
             Attributes
         =========================================
         */
-
-        bool IsWater, IsHill, IsFood;
-        int HillPlayer, TurnsInFog;
+        
+        // Whether the Square is a known Water tile
+        // This information never gets Reset 
+        // Unknown tiles (in the Fog Of War) are not Water tiles until discovered
+        bool IsWater;
+        // Whether the Square is a visible Hill tile
+        bool IsHill;
+        // Whether the Square is a visible Food tile
+        bool IsFood;
+        // Hill's Player id
+        // = -1 If the tile is not a Hill
+        // = 0 If it's an Ally Hill
+        // > 0 If it's an Enemy Hill
+        int HillPlayer;
+        // Turns spent in the Fog Of War
+        // = 0 For visible tiles
+        int TurnsInFog;
+        // Info about the Ant that's on the Square
+        // = nullptr when there is no Ant
         Ant* Ant;
-
         
         /*
         =========================================
