30125db6bd8190d3a3c5e389ca157ff3b0f94146
Alex Vu <alex.vu.auditeur@lecnam.net>
2023-05-01 19:23:39 +0200

    add comments to combat evaluator

--- src/combat/CombatEvaluator.h
+++ src/combat/CombatEvaluator.h
@@ -4,6 +4,7 @@
 struct State;
 #include "CombatState.h"
 
+// Class used to evaluate the best move for a given combat configuration
 class CombatEvaluator
 {    
     public:
@@ -12,11 +13,17 @@ class CombatEvaluator
             Attributes
         =========================================
         */
-
+        
+        // Pointer to the game's current state
         State *GameState;
+        // Pointer to the combat state which is being evaluated
         CombatState *CurrentCombatState;
+        // Stack of the best combination of moves which have been found so far
+        // in the format <point to Ant, move direction index>
         std::stack<std::pair<Ant*, int>> BestMoves;
+        // Best evaluated gain found so far
         int BestGain;
+        // Number of iterations made after initialization a new combat
         int TotalIterations;
 
         /*
@@ -25,13 +32,29 @@ class CombatEvaluator
         =========================================
         */
 
+        // Creates an evaluator for combats and give it the pointer to the game's state
         CombatEvaluator(State *gameState);
+        // Attempts to computes the best move for a given starting combat state
+        // where one ally group combats one enemy group.
+        // The algorithm used is an adaptation of the alpha-beta algorithm, with limited iterations.
+        // The objective is minimizing the possible enemy gain while making ally moves.
         void ComputeBestMove(CombatState *startingCombatState);
+        // Evaluates a combat state's gain
         int EvaluateCurrentCombatState();
 
     private:
+        /*
+        =========================================
+            Methods
+        =========================================
+        */
+
+        // Initializes the combat state
         void Initialize(CombatState *startingCombatState);
+        // Attempts to compute all possible moves for remaining unmoved allies
         void ComputeNextAllyMove();
+        // Attempts to computes all possible moves for remaining unmoved enemies
+        // and attempts to return the best possible enemy gain for given configuration
         int ComputeNextEnemyMove();
 };
 
